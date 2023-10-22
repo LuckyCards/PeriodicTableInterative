b5eaa3cd5039a8f1ac8d366d9ac6b5a23abe37eb
Luiz Claudio <68889180+DinowSauron@users.noreply.github.com>
2023-10-22 00:20:37 -0300

    Refatoração com funções

--- searchElement.js
+++ searchElement.js
@@ -1,61 +1,92 @@
-var buttonSearch = document.getElementById("search-button");
-var b = document.getElementById("search-button-icon");
+const buttonSearch = document.getElementById("search-button");
+const icon = document.getElementById("search-button-icon");
 var rotated = false;
 
 buttonSearch.addEventListener("click", () => {
-  let title = document.querySelector("h1");
-  let searchBar = document.getElementById("search-bar");
+  const searchBar = document.getElementById("search-bar");
+
   if (window.innerWidth <= 780) {
-    if (!rotated) {
-      b.style.transform = "rotateZ(180deg)";
-      title.style.transform = "translateY(80px)";
-      searchBar.style.transform = "translateY(0px)";
-      rotated = true;
-    } else {
-      b.style.transform = "rotateZ(0deg)";
-      title.style.transform = "translateY(0px)";
-      searchBar.style.transform = "translateY(-80px)";
-      searchBar.value = "";
-      rotated = false;
-    }
-    searchBar.addEventListener("input", () => {
-      let elements = document.querySelectorAll("#elements-blocks li");
-      let searchText = searchBar.value.toLowerCase();
-
-      elements.forEach((e) => {
-        let text = e.textContent.toLowerCase();
-
-        if (text.includes(searchText)) {
-          e.style.display = "flex";
-        } else {
-          e.style.display = "none";
-        }
-      });
+    animateRotationButtonResponsive();
+    searchBar.addEventListener("input", (e) => {
+      unselect()
+      filterElements(e, "display: flex;", "display: none;")
+      resizeGridSize();
     });
+    
   } else {
-    if (!rotated) {
-      b.style.transform = "rotateZ(180deg)";
-      searchBar.style.transform = "scaleX(1)";
-      rotated = true;
-    } else {
-      b.style.transform = "rotateZ(0deg)";
-      searchBar.style.transform = "scaleX(0)";
-      searchBar.value = "";
-      rotated = false;
-    }
-    searchBar.addEventListener("input", () => {
-      let elements = document.querySelectorAll("#elements-blocks li");
-      let searchText = searchBar.value.toLowerCase();
-
-      elements.forEach((e) => {
-        let text = e.textContent.toLowerCase();
-
-        if (text.includes(searchText)) {
-          e.style.filter = "";
-        } else {
-          e.style.filter = "grayscale(90%)";
-        }
-      });
+    animateRotationButtonDefault();
+    searchBar.addEventListener("input", (e) => {
+      unselect()
+      filterElements(e, "filter: ;", "filter: grayscale(90%)")
     });
   }
 });
+
+function resizeGridSize() {
+  const searchBar = document.getElementById("search-bar");
+  let activatedElements = 0;
+  
+  validElementsByText({
+    textValue: searchBar.value,
+    cbValid: () => {activatedElements++}
+  })
+  
+  const grid = document.querySelector("#elements-blocks");
+  console.log(grid.style.gridTemplateColumns)
+  grid.style.gridTemplateColumns = `repeat(${activatedElements}, 80px)`;
+}
+
+function filterElements(e, styleActive, styleDesactived) {
+  validElementsByText({
+    textValue: e.target.value,
+    cbValid: (elm) => {elm.style = styleActive}, 
+    cbInvalid: (elm) => {elm.style = styleDesactived}
+  });
+}
+
+function validElementsByText({textValue, cbValid = ()=>{}, cbInvalid = ()=>{}}) {
+  const searchText = textValue.toLowerCase();
+  const elements = document.querySelectorAll("#elements-blocks li");
+  elements.forEach((element) => {
+    const text = element.textContent.toLowerCase();
+    
+    if (text.includes(searchText)) {
+      cbValid(element);
+    } else {
+      cbInvalid(element);
+    }
+  });
+}
+
+function animateRotationButtonResponsive() {
+  const searchBar = document.getElementById("search-bar");
+  const title = document.querySelector("h1");
+
+  if (!rotated) {
+    icon.style.transform = "rotateZ(180deg)";
+    title.style.transform = "translateY(80px)";
+    searchBar.style.transform = "translateY(0px)";
+    rotated = true;
+  } else {
+    icon.style.transform = "rotateZ(0deg)";
+    title.style.transform = "translateY(0px)";
+    searchBar.style.transform = "translateY(-80px)";
+    searchBar.value = "";
+    rotated = false;
+  }
+}
+
+function animateRotationButtonDefault() {
+  const searchBar = document.getElementById("search-bar");
+
+  if (!rotated) {
+    icon.style.transform = "rotateZ(180deg)";
+    searchBar.style.transform = "scaleX(1)";
+    rotated = true;
+  } else {
+    icon.style.transform = "rotateZ(0deg)";
+    searchBar.style.transform = "scaleX(0)";
+    searchBar.value = "";
+    rotated = false;
+  }
+}
